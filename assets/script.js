2213ae72f3b55476df03c7f5241578b2901d8c8e
ulisesd404 <144632800+ulisesd404@users.noreply.github.com>
2023-12-27 13:39:50 -0800

    api call working

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,18 @@
 var submitBtn = document.querySelector('#submitBtn')
 
-function searchApi(city) {
-    var weatherUrl = 'api.openweathermap.org/data/2.5/forecast'
-    var appId = '1e9e1ad97fa826aaf8223cfe5d47eb00'
-    if (city) {
-        weatherUrl = 'api.openweathermap.org/data/2.5/forecast?q='+ cityInput + '&appid='+ appId;
-    }
-    fetch(weatherUrl)
-    .then(function (response) {
-        if (!response.ok) {
-          throw response.json();
-        }
-        return response.json();
-    })
-    .then()
+
+
+function getWeather(cityInput){
+    var request = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityInput + '&appid=1e9e1ad97fa826aaf8223cfe5d47eb00'
+    
+    fetch(request)
+        .then(function (response){
+            return response.json();
+        })
+        .then(function (data){
+            console.log(data);
+        })
 }
 
-function searchSubmit(event) {
-    event.preventDefault();
 
-    var cityInput = document.querySelector('#textInput').value;
-
-    if (!cityInput){
-        console.error('You need to input a city!');
-        return;
-    }
-    searchApi(cityInput)
-}
-
-submitBtn.addEventListener('submit',searchSubmit);
+getWeather();
