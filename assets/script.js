036ed047d801983e2540ce9b816a2ecb9434f2c9
ulisesd404 <144632800+ulisesd404@users.noreply.github.com>
2023-12-27 19:50:26 -0800

    almost all of the writing down is done, just need to finish some tweaks

--- assets/script.js
+++ assets/script.js
@@ -9,20 +9,56 @@ function getWeather(cityInput){
     
     fetch(request)
         .then(function (response) {
-            if (!response.ok) {
-                throw response.json();
-            }
             return response.json();
         })
 
         .then(function (data) {
             console.log(data);
-})
-};     
+            printResults(data)
+        })
+        .catch(function (error){
+            console.error(error);
+        })
+}
 
-function printResults(){
+function printResults(data){
+    var resultBody = document.querySelector('.currentDayWeather') 
+    //big card
     var bigCard = document.createElement('div');
-    bigCard.classList.add('card',)
+    bigCard.classList.add('currentDayWeather');
+    //big date and city
+    var dateBig = document.createElement('h2 strong');
+    dateBig.classList.add('bigDate');
+    var currentDay = dayjs().format('dd/MM/DD');
+    dateBig.textContent = cityInput + currentDay;
+
+    //big icon
+    var currentDayIcon = document.createElement('img');
+    currentDayIcon.classList.add('bigIcon');
+    var currentIconCode = response.current.weather[0].icon;
+    currentDayIcon.attr('src','`https://openweathermap.org/img/wn/' + currentIconCode  + '@2x.png')
+
+    //temperature
+    var currentDayTemp = document.createElement('p');
+    currentDayTemp.classList.add('bigText');
+    var currentTemp = response.current.temp;
+    currentDayTemp.text('Temperature: ' + currentTemp);
+
+    //wind
+    var currentDayWind = document.createElement('p');
+    currentDayWind.classList.add('bigText');
+    var currentWind = response.current.wind.speed;
+    currentDayWind.textContent = 'Wind: ' + currentWind + 'MPH';
+
+    //humidity
+
+    var currentDayHum = document.createElement('p');
+    currentDayHum.classList.add('bigText');
+    var currentHum = response.current.humidity;
+    currentDayHum.textContent = 'Humitity: ' + currentHum + '%';
+
+    bigCard.append(dateBig, currentDayIcon, currentDayTemp, currentDayWind, currentDayHum)
+    resultBody.append(bigCard);
 }
       //recently searched tab
 function printPrevious(cityInput){
